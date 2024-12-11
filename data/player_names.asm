475e7d2f41fcd0a68c167442a14286ce5ba82b3f
AngelofSilence1213 <182548478+AngelofSilence1213@users.noreply.github.com>
2024-12-11 03:42:31 +0000

    Properly capitalized player names

--- data/player_names.asm
+++ data/player_names.asm
@@ -1,20 +1,20 @@
 DefaultNamesPlayer:
-	db   "NEW NAME"
-	next "YELLOW"
-	next "ASH"
-	next "JACK"
+	db   "New Name"
+	next "Yellow"
+	next "Ash"
+	next "Jack"
 	db   "@"
 
 DefaultNamesGirl:
-	db   "NEW NAME"
-	next "GREEN"
-	next "LEAF"
-	next "AMANDA"
+	db   "New Name"
+	next "Blue"
+	next "Leaf"
+	next "Amanda"
 	db   "@"
 
 DefaultNamesRival:
-	db   "NEW NAME"
-	next "BLUE"
-	next "GARY"
-	next "JOHN"
+	db   "New Name"
+	next "Green"
+	next "Gary"
+	next "John"
 	db   "@"
